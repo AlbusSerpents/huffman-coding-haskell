88fec95e5d7f5c9edf0c6cfa43e0a9d3059ac1b9
D.Naydenov <d.naydenov@futurist-labs.com>
2019-08-21 01:30:47 +0300

    Moved the occurances related things in a separate module

--- Main.hs
+++ Main.hs
@@ -1,51 +1,8 @@
 module Main where
 
-import           Data.Function (on)
-import           Data.List     (sort)
+import qualified Occurances as O
 
 main :: IO ()
-main = putStrLn "Hello World"
-
-type OccuranceMap a = [(a, Int)]
-
-makeOccurenceMap
-  :: (Eq a)
-  => [a] -> OccuranceMap a
-makeOccurenceMap = foldl find []
-  where
-    find [] element = [(element, 1)]
-    find (x@(symbol, count):xs) element
-      | symbol == element = (symbol, count + 1) : xs
-      | otherwise = x : find xs element
-
-data OccuranceTree a
-  = Leaf { element :: a
-         , count   :: Int}
-  | Vertex { left  :: OccuranceTree a
-           , right :: OccuranceTree a
-           , count :: Int}
-  deriving (Eq, Show)
-
-instance (Eq a) =>
-         Ord (OccuranceTree a) where
-  compare = compare `on` count
-
-buildOccurenceTree
-  :: (Eq a)
-  => OccuranceMap a -> OccuranceTree a
-buildOccurenceTree = reduceTrees . map (uncurry Leaf)
-
-reduceTrees
-  :: (Eq a)
-  => [OccuranceTree a] -> OccuranceTree a
-reduceTrees [x] = x
-reduceTrees trees = reduceTrees $ combineTrees t1 t2 : rest
-  where
-    ([t1, t2], rest) = splitAt 2 $ sort trees
-
-combineTrees
-  :: (Eq a)
-  => OccuranceTree a -> OccuranceTree a -> OccuranceTree a
-combineTrees left right = Vertex left right $ sumCounts left right
-  where
-    sumCounts = (+) `on` count
+main = do
+  input <- getLine
+  print $ O.buildOccurenceTree input
---
+++ Occurances.hs
@@ -0,0 +1,49 @@
+module Occurances
+  ( buildOccurenceTree
+  , OccuranceTree(..)
+  ) where
+
+import           Data.Function (on)
+import           Data.List     (sort)
+
+data OccuranceTree a
+  = Leaf { element :: a
+         , count   :: Int}
+  | Vertex { left  :: OccuranceTree a
+           , right :: OccuranceTree a
+           , count :: Int}
+  deriving (Eq, Show)
+
+instance (Eq a) =>
+         Ord (OccuranceTree a) where
+  compare = compare `on` count
+
+type OccuranceMap a = [(a, Int)]
+
+buildOccurenceTree
+  :: (Eq a)
+  => [a] -> OccuranceTree a
+buildOccurenceTree = reduceTrees . map (uncurry Leaf) . buildOccurenceMap
+
+buildOccurenceMap
+  :: (Eq a)
+  => [a] -> OccuranceMap a
+buildOccurenceMap = foldl addOccurance []
+  where
+    addOccurance [] el = [(el, 1)]
+    addOccurance (x@(symbol, occurances):xs) el
+      | symbol == el = (symbol, occurances + 1) : xs
+      | otherwise = x : addOccurance xs el
+
+reduceTrees
+  :: (Eq a)
+  => [OccuranceTree a] -> OccuranceTree a
+reduceTrees [x] = x
+reduceTrees trees = reduceTrees $ combineTrees t1 t2 : rest
+  where
+    ([t1, t2], rest) = splitAt 2 $ sort trees
+
+combineTrees :: OccuranceTree a -> OccuranceTree a -> OccuranceTree a
+combineTrees a b = Vertex a b $ addCounts a b
+  where
+    addCounts = (+) `on` count

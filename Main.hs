f7f065e44e68c4c989bd33f5f473f994f3c3c471
D.Naydenov <d.naydenov@futurist-labs.com>
2019-08-21 00:53:22 +0300

    Implemented an occurance map

--- Main.hs
+++ Main.hs
@@ -2,3 +2,15 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello World"
+
+type OccuranceMap a = [(a, Int)]
+
+makeOccurenceMap
+  :: (Eq a)
+  => [a] -> OccuranceMap a
+makeOccurenceMap = foldl find []
+  where
+    find [] element = [(element, 1)]
+    find (x@(symbol, count):xs) element
+      | symbol == element = (symbol, count + 1) : xs
+      | otherwise = x : find xs element

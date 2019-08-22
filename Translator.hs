e45ce946dff0ed8628cc98fbde366655c1b70ef6
D.Naydenov <d.naydenov@futurist-labs.com>
2019-08-22 04:24:51 +0300

    Implemented a module for transforming ciphers to syntax trees

---
+++ Translator.hs
@@ -0,0 +1,28 @@
+module Translator
+  ( buildSynraxTree
+  ) where
+
+import           Control.Arrow (second, (***))
+import           Data.List     (sortOn, span)
+import           Syntax        (Cipher, Symbol (..), SyntaxTree (..))
+
+instance Ord Symbol where
+  compare One Zero = GT
+  compare Zero One = LT
+  compare _ _      = EQ
+
+buildSynraxTree :: Cipher a -> SyntaxTree a
+buildSynraxTree [(info, code)] = DataPoint info
+buildSynraxTree cipher =
+  uncurry Branch $
+  onPair buildSynraxTree $
+  onPair removeFirst $ span startsWithZero $ sortOn snd cipher
+  where
+    startsWithZero = (== Zero) . head . snd
+    removeFirst = map (second tail)
+
+onPair :: (a -> b) -> (a, a) -> (b, b)
+onPair f = f *** f
+
+combine :: SyntaxTree a -> SyntaxTree a -> SyntaxTree a
+combine = Branch
